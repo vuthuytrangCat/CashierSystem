2234f3a3c45ac04271d402d392f064d37fe31498
vuthuytrangCat <132036453+vuthuytrangCat@users.noreply.github.com>
2023-05-01 22:57:52 +0900

    udpate

--- lib/item_prices.rb
+++ lib/item_prices.rb
@@ -3,41 +3,32 @@
 require "yaml"
 # :nodoc:
 class ItemPrices
-  p_read = File.open("../assets/products.yml")
-products_list = YAML.load(p_read.read())
-
-  r_read = File.open("../assets/rules.yml")
-rules_list = YAML.load(r_read.read())
+products_list =  
+YAML.load_file(File.join(File.dirname(__FILE__), '../assets/products.yml'))
+rules_list = 
+YAML.load_file(File.join(File.dirname(__FILE__), '../assets/rules.yml'))
 puts "FreeRule (buy #{rules_list['FreeRule']['number_of_items']} get #{rules_list['FreeRule']['number_of_items']} free, pay #{rules_list['FreeRule']['sale_price']})"
 
 puts "ReducedPriceRule (buy more than #{rules_list['ReducedPriceRule']['number_of_items']} , pay a different price　£#{rules_list['ReducedPriceRule']['different_price']})"  
 
 puts "FractionPriceRule (buy more than #{rules_list['FractionPriceRule']['number_of_items']}, pay #{rules_list['FractionPriceRule']['sale_percent'].to_s+ "%"} of the original price)"
 
-#puts products_list['Green Tea']['unit_price']
-#puts rules_list['FreeRule']['number_of_items']
-#puts rules_list['FreeRule']['sale_price']
-#puts  rules_list['FractionPriceRule']['number_of_items']
-#puts   rules_list['FractionPriceRule']['sale_percent']
-#puts   rules_list['ReducedPriceRule']['number_of_items']
-#puts rules_list['ReducedPriceRule']['different_price']
-
-  CURRENT_PRICES = {
+CURRENT_PRICES = {
     'gr1' => {
       'unit_price' => products_list['Green Tea']['unit_price'],
       'selling_price' => {
         'number_of_items' => rules_list['FreeRule']['number_of_items'],
         'sale_price' => rules_list['FreeRule']['sale_price'],
-        #'different_price' => {},
-        #'sale_percent' =>{}
+        'different_price' => {},
+        'sale_percent' =>{}
       }
     },
     'sr1' => {
       'unit_price' => products_list['Strawberries']['unit_price'],
       'selling_price' => {
         'number_of_items' => rules_list['FractionPriceRule']['number_of_items'],
-        #'sale_price' => {},
-        #'different_price'=>{},
+        'sale_price' => {},
+        'different_price'=>{},
         'sale_percent' => rules_list['FractionPriceRule']['sale_percent']
       }
     },
@@ -45,9 +36,9 @@ puts "FractionPriceRule (buy more than #{rules_list['FractionPriceRule']['number
       'unit_price' => products_list['Coffee']['unit_price'],
       'selling_price' => {
         'number_of_items' => rules_list['ReducedPriceRule']['number_of_items'],
-        #'sale_price' =>{},
+        'sale_price' =>{},
         'different_price' => rules_list['ReducedPriceRule']['different_price'],
-        #'sale_percent' =>{}
+        'sale_percent' =>{}
       }
     }
     
